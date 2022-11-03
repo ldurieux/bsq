4260670eb463749cae75ad23e5a6771142bbeff7
Louis Durieux <loudurserveur@gmail.com>
2022-11-03 21:41:04 +0100

    fix norminette

--- srcs/solver/add_line.c
+++ srcs/solver/add_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   add_line.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldurieux <ldurieux@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 21:23:43 by ldurieux          #+#    #+#             */
+/*   Updated: 2022/11/03 21:23:45 by ldurieux         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "solver.h"
 
 static int	first_line(t_solver *solver, const char *line)
@@ -9,7 +21,31 @@ static int	first_line(t_solver *solver, const char *line)
 		return (0);
 	idx = (size_t)-1;
 	while (line[++idx])
+	{
 		solver->last_line[idx] = line[idx] == solver->chrs[WALL_IDX];
+		if (solver->last_line[idx] > solver->best_point_size)
+		{
+			solver->best_point.y = solver->cur_line;
+			solver->best_point.x = idx;
+			solver->best_point_size = solver->last_line[idx];
+		}
+	}
+	return (1);
+}
+
+static int	init(t_solver *solver, const char *line, size_t *idx, char *wall)
+{
+	if (!solver)
+		return (0);
+	solver->cur_line++;
+	if (!solver->last_line)
+	{
+		first_line(solver, line);
+		return (0);
+	}
+	wall = solver->chrs[WALL_IDX];
+	idx = (size_t)0;
+	solver->last_line[0] = (line[idx] == wall) * solver->last_line[0];
 	return (1);
 }
 
@@ -18,14 +54,8 @@ void	solver_add_line(t_solver *solver, const char *line)
 	size_t	idx;
 	char	wall;
 
-	if (!solver)
+	if (!init(solver, line, &idx, &wall))
 		return ;
-	solver->cur_line++;
-	if (!solver->last_line)
-		return (first_line(solver, line));
-	wall = solver->chrs[WALL_IDX];
-	idx = (size_t)0;
-	solver->last_line[0] = (line[idx] == wall) * solver->last_line[0];
 	while (line[++idx])
 	{
 		if (line[idx] == wall)
--- srcs/solver/print_solution_fd.c
+++ srcs/solver/print_solution_fd.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_solution_fd.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldurieux <ldurieux@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 21:23:50 by ldurieux          #+#    #+#             */
+/*   Updated: 2022/11/03 21:23:51 by ldurieux         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "solver.h"
 
 static void	skip_line(int fd)
@@ -8,6 +20,15 @@ static void	skip_line(int fd)
 		;
 }
 
+static void	init(t_solver *solver, int fd, t_rect *rect, t_point *pos)
+{
+	*rect = solver_get_solution(solver);
+	rect->width = rect->x + rect->width;
+	rect->height = rect->y + rect->height;
+	skip_line(fd);
+	pos->y = (uint32_t)-1;
+}
+
 void	solver_print_solution_fd(t_solver *solver, int fd)
 {
 	t_rect	rect;
@@ -16,22 +37,18 @@ void	solver_print_solution_fd(t_solver *solver, int fd)
 
 	if (!solver)
 		return ;
-	rect = solver_get_solution(solver);
-	rect.width = rect.x + rect.width;
-	rect.height = rect.y + rect.height;
-	skip_line(fd);
-	pos.y = (uint32_t)-1;
+	init(solver, fd, &rect, &pos);
 	while (++pos.y)
 	{
-		pos.x = (uint32_t)-1;
+		pos.x = (uint32_t) - 1;
 		while (++pos.x)
 		{
 			if (read(fd, &buf, 1) <= 0)
 				return ;
 			if (buf == '\n')
 				break ;
-			if (pos.x >= rect.x && pos.x < rect.width &&
-				pos.y >= rect.y && pos.y < rect.height)
+			if (pos.x >= rect.x && pos.x < rect.width
+				&& pos.y >= rect.y && pos.y < rect.height)
 				write(STDOUT, solver->chrs + SOLUTION_IDX, 1);
 			write(STDOUT, &buf, 1);
 		}
